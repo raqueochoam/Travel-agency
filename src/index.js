214409c715e61c9cabf6ca28732cbc56e6388126
Edgar <edgarvlzm26@gmail.com>
2021-11-21 14:57:49 -0600

    commit Node_modules

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Heading from './Heading';
+import Heading from './Heading.js';
 
 ReactDOM.render(
   <React.StrictMode>
