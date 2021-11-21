fc07d9b2be98526ba76d1821365639c9ec30aae8
Jorgepro89 <0237032@up.edu.mx>
2021-11-21 14:47:06 -0600

    commit test jorge

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.render(
 );
 
 //Test Edgar 2
-
+//Test Jorge
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
