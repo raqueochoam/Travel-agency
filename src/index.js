34bd3e9c0690442e39039fe261782ad9f541b8c9
Edgar <edgarvlzm26@gmail.com>
2021-11-21 14:09:29 -0600

    update Test 2 Edgar

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-//Test Edgar
+//Test Edgar 2
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
